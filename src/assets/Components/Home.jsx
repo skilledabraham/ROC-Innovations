06f0ae23dcdd21d3e48d537e7d78a0f8310d49ac
Ibrahim Abraham <wealthabram@gmail.com>
2025-10-16 22:39:58 +0100

    Posi Commit2

--- src/assets/Components/Home.jsx
+++ src/assets/Components/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
   return (
     <div className="w-screen p-0 m-0 overflow-hidden">
       <Header></Header>
-      <div className="text-center mt-0 py-[1em] flex justify-center items-center mt-5">
+      <div className="text-center  py-[1em] flex justify-center items-center mt-5">
         <img src={Posi} className="border-orange-600 border-4 h-[70vh]" />
         <div className=" hover:bg-zinc-100 hover:shadow-sm rounded hover:shadow-gray-500 h-[150%] w-[30%] p-10 m-10">
           <h1 className="text-5xl font-bold text-left ml-10">Posi Bello!</h1>
